57cd1f10d8ac894efdc37cc6abd23c53907cd675
Phát Trịnh Tấn <trinhtanphat2403@gmail.com>
2026-04-01 13:30:25 +0700

    fix(android): enable BuildConfig generation for debug build

--- SlapMac-Android/app/build.gradle.kts
+++ SlapMac-Android/app/build.gradle.kts
@@ -21,6 +21,10 @@ android {
         }
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
